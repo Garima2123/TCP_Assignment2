8916dc54a966ed5cd64dc4bf2afdb9861663f97e
Garima Kumari <70741733+Garima2123@users.noreply.github.com>
2021-02-08 17:31:44 +0530

    Update Server.go

--- Server.go
+++ Server.go
@@ -5,116 +5,65 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
-	"time"
 )
 
-//var temp string
+var count = 0
 
-func enqueue(queue []string, element string) []string {
-	queue = append(queue, element) // Simply append to enqueue.
-	//fmt.Println("Enqueued:", element)
-	return queue
+func handleConnection(c net.Conn, count int) {
+	//fmt.Print("client. ")
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		temp := strings.TrimSpace(string(netData))
+		if temp == "STOP" {
+			break
+		}
+		counter := strconv.Itoa(count)
+		fmt.Println("client= ", counter, ": ", temp)
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("server= ", counter, ": ")
+		text, _ := reader.ReadString('\n')
+		//user input is sent to the TCP server over the network
+		fmt.Fprintf(c, text+"\n")
+		//
+		// c.Write([]byte(string(counter)))
+	}
+	c.Close()
 }
 
 func main() {
-	q := make(map[string][]string, 10)
-
 	arguments := os.Args
 	if len(arguments) == 1 {
-		fmt.Println("Please provide port number")
+		fmt.Println("Please provide a port number!")
 		return
 	}
 
 	PORT := ":" + arguments[1]
-	l, err := net.Listen("tcp", PORT)
+	l, err := net.Listen("tcp4", PORT)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer l.Close()
 
-	c, err := l.Accept()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	var queue []string
-	var username string
 	for {
-		newdata, err := bufio.NewReader(c).ReadString('\n')
+		c, err := l.Accept()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		if strings.TrimSpace(string(newdata)) == "STOP" {
-			fmt.Println("Exiting TCP server!")
-			return
-		}
+		count++
+		go handleConnection(c, count)
 
-		fmt.Print("Received message -> ", string(newdata))
-		t := time.Now()
-		myTime := t.Format(time.RFC3339) + "\n"
-		c.Write([]byte(myTime))
-
-		if strings.TrimSpace(string(newdata)) == "NEW" {
-			fmt.Println("New user")
-
-			//username := string(newdata)
-			newdata, err := bufio.NewReader(c).ReadString('\n')
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			username = string(newdata)
-			q[username] = queue
-
-		} else {
-			queue = append(queue, string(newdata))
-			//queue = enqueue(queue, string(newdata))
-			q[username] = queue
-		}
-
-		if strings.TrimSpace(string(newdata)) == "BRAKE" {
-			newdata, err := bufio.NewReader(c).ReadString('\n')
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println("Command:", string(newdata))
-			queue = nil
-			//queue = enqueue(queue, string(newdata))
-			//
-		}
-
-		if strings.TrimSpace(string(newdata)) == "PRINT" {
-			//fmt.Println("Queue:", q)
-			for key, value := range q {
-				value = value[:len(value)-1]
-				fmt.Printf("%s value is %v\n", key, value)
-
-			}
-			//queue = enqueue(queue, string(newdata))
-			//
-		}
-
-		if strings.TrimSpace(string(newdata)) == "FIRST" {
-			//fmt.Println("Queue:", q)
-			newdata, err := bufio.NewReader(c).ReadString('\n')
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			username = string(newdata)
-
-			for key, value := range q {
-				if key == username {
-					fmt.Println("First message of ", key, " is ", value[:1])
-				}
-
-			}
-			//queue = enqueue(queue, string(newdata))//
-			
-		}
 	}
 }
+
+
+	
